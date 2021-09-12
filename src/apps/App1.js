5941b4b13021f5d30839cd36246220331cd2590e
LasseDomdal <domdal.lasse@gmail.com>
2021-09-12 20:54:55 +0200

    commit

---
+++ src/apps/App1.js
@@ -0,0 +1,16 @@
+import "./App1.css";
+
+function App1() {
+
+    return (
+        <div className="App">
+            <header>
+                this i header
+            </header>
+            <footer>
+                this is a footer
+            </footer>
+        </div>
+    )
+}
+export default App1
---
+++ src/apps/App2.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+class App2 extends react.Component {
+    render() {
+        return(
+            <h1>hello world2!</h1>
+        )
+    }
+}
+
+export default App2;
